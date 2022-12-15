9726db0ef119bbd0f6c78f1b8bee80b63269beb1
mayday1004 <iko7695@gmail.com>
2022-12-15 17:40:21 +0800

    不同頻道可以獲取不同主題的推文，增加停止推文功能

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 import datetime, asyncio, os
 import nextcord
 from nextcord.ext import commands
-from tweepy_setup import search_result
+from tweepy_setup import check_tweets
 from dotenv import load_dotenv
 load_dotenv()
-
-auto_message_switch = False
-
 intents = nextcord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-#設置頻道要獲取的推文:$初始化 <頻道ID> <欲搜尋的關鍵字>
+current_active_channel = []
+
+#設置頻道要獲取的推文:$初始化 <頻道ID> <欲搜尋的關鍵字> <獲取讚數以上推文>
 @bot.command(name="初始化")
-async def SendMessage(ctx, room_id, keyword):
+async def SendMessage(ctx, room_id, keyword, faves):
     await ctx.send(f"頻道ID: {room_id} 無誤，開始獲取推文")
-    await auto_message(int(room_id), keyword)
+    await auto_message(str(ctx.command), int(room_id), keyword, int(faves))
 
+#設置頻道停止獲取推文:$停止推文 <頻道ID>
+@bot.command(name="停止推文")
+async def SendMessage(ctx, room_id):
+    await auto_message(str(ctx.command), int(room_id), " ", 50)
 
-async def auto_message(room_id, keyword):
-    while True:
-        get_tweets = search_result(keyword)
+#執行發送推文
+async def auto_message(command, room_id, keyword, faves=50):
+    if command == "停止推文":
+        current_active_channel.remove(room_id)
+        channel = bot.get_channel(room_id)
+        await channel.send(f"頻道ID: {room_id} 已暫停獲取推文")
+    else:
+        current_active_channel.append(room_id)
+
+    while room_id in current_active_channel:
+        get_tweets = check_tweets(keyword, faves)
         channel = bot.get_channel(room_id)
         if len(get_tweets) > 0:
             for tweet in get_tweets:
                 await channel.send(tweet)
+        #設置推文發送間隔
         now = datetime.datetime.now()
-        then = now+datetime.timedelta(hours = 1)
+        then = now+datetime.timedelta(hours = 2)
         wait_time = (then-now).total_seconds()
         await asyncio.sleep(wait_time)
 
 @bot.event
 async def on_ready():
     print(f"Loggined in as: {bot.user.name}")
+    #機器人狀態
     game = nextcord.Game("馬應龍的推特")
     await bot.change_presence(status=nextcord.Status.online, activity=game)
 
--- tweepy_setup.py
+++ tweepy_setup.py
@@ -17,24 +17,29 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-result_lists = set()
-def search_result(keyword):
-    clone_result_lists = result_lists.copy()
+keyword_dict = {}
+
+def search_tweets(keyword, faves):
+    search_results = []
     #使用 tweepy 進行推特搜索
-    search_word = f"{keyword} min_faves:50 filter:links -filter:replies"
+    search_word = f"{keyword} min_faves:{faves} filter:media -filter:replies -filter:retweets"
     for tweets in api.search_tweets(q=search_word, result_type="recent", count=100):
         human_time = str(tweets.created_at).split("+")[0]
         tweet_result_format = f"{human_time}  |  https://twitter.com/user/status/{tweets.id}"
-        
-        if len(clone_result_lists) == 0:
-            #第一次搜尋
-            result_lists.add(tweet_result_format)
-        else:
-            #第二次以上搜尋，判斷set有沒有已經送出的推文? 有>移除，沒有>新增
-            if tweet_result_format in result_lists:
-                result_lists.remove(tweet_result_format)
-            else:
-                result_lists.add(tweet_result_format)
+        search_results.insert(0, tweet_result_format)
+    return search_results
 
-    return result_lists
+def check_tweets(keyword, faves):
+    search_results = search_tweets(keyword, faves)
+    if keyword not in keyword_dict:
+        #第一次搜尋
+        keyword_dict[keyword] = search_results
+        return search_results
+    else:
+        #第二次或以上搜尋，篩選掉已經發過的內容
+        filtered_search_results = search_results.copy()
+        for tweet in keyword_dict[keyword]:
+            filtered_search_results.remove(tweet)
             
+        keyword_dict[keyword] = search_results
+        return filtered_search_results
