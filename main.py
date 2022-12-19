e022d457f6568a3d2635b69007ebdb113d9dbe22
mayday1004 <iko7695@gmail.com>
2022-12-20 00:24:30 +0800

    優化搜尋結果

--- main.py
+++ main.py
@@ -1,53 +1,80 @@
-import datetime, asyncio, os
+import datetime
+import asyncio
+import os
 import nextcord
 from nextcord.ext import commands
-from tweepy_setup import check_tweets
+from tweepy_setup import check_tweets, stop_tweets
 from dotenv import load_dotenv
+
 load_dotenv()
 intents = nextcord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
-current_active_channel = []
+current_active_channel = set()
 
-#設置頻道要獲取的推文:$初始化 <頻道ID> <欲搜尋的關鍵字> <獲取讚數以上推文>
-@bot.command(name="初始化")
-async def SendMessage(ctx, room_id, keyword, faves):
-    await ctx.send(f"頻道ID: {room_id} 無誤，開始獲取推文")
-    await auto_message(str(ctx.command), int(room_id), keyword, int(faves))
-
-#設置頻道停止獲取推文:$停止推文 <頻道ID>
-@bot.command(name="停止推文")
-async def SendMessage(ctx, room_id):
-    await auto_message(str(ctx.command), int(room_id), " ", 50)
-
-#執行發送推文
-async def auto_message(command, room_id, keyword, faves=50):
-    if command == "停止推文":
-        current_active_channel.remove(room_id)
-        channel = bot.get_channel(room_id)
-        await channel.send(f"頻道ID: {room_id} 已暫停獲取推文")
+# 確認機器人有在運行
+@bot.command(name="log")
+async def SendMessage(ctx):
+    await ctx.send(f"活動中頻道列表 : {current_active_channel}")
+    if ctx.channel.id in current_active_channel:
+        await ctx.send("當前頻道正接收推文")
     else:
-        current_active_channel.append(room_id)
+        await ctx.send("當前頻道沒有接收任何推文")
 
+
+# 設置頻道要獲取的推文:$推特搜尋 <欲搜尋的關鍵字> <獲取讚數以上推文>
+@bot.command(name="推特搜尋")
+async def SendMessage(ctx, keyword, exclude_words, faves):
+    keyword = keyword.replace("!", "#")
+    exclude_words = exclude_words.split(",")
+    current_active_channel.add(int(ctx.channel.id))
+    await ctx.send(f"頻道ID: {ctx.channel.id} 開始獲取推文")
+    await auto_message(int(ctx.channel.id), keyword, exclude_words, int(faves))
+
+
+# 設置頻道停止獲取推文:$停止推文
+@bot.command(name="停止推文")
+async def SendMessage(ctx):
+    current_active_channel.remove(int(ctx.channel.id))
+    stop_tweets(int(ctx.channel.id))
+    await ctx.send(f"頻道ID: {ctx.channel.id} 已暫停獲取推文")
+
+
+# 刪除頻道消息，至多100則:
+@commands.has_permissions(manage_messages=True)
+@bot.command(name="清理對話")
+async def SendMessage(ctx, num=100):
+    await ctx.channel.purge(limit=num)
+
+
+# 執行發送推文
+async def auto_message(room_id, keyword, exclude_words, faves):
     while room_id in current_active_channel:
-        get_tweets = check_tweets(keyword, faves)
+        get_tweets = check_tweets(room_id, keyword, exclude_words, faves)
         channel = bot.get_channel(room_id)
         if len(get_tweets) > 0:
             for tweet in get_tweets:
                 await channel.send(tweet)
-        #設置推文發送間隔
+        # 設置推文發送間隔
         now = datetime.datetime.now()
-        then = now+datetime.timedelta(hours = 2)
-        wait_time = (then-now).total_seconds()
+        then = now + datetime.timedelta(hours=1)
+        wait_time = (then - now).total_seconds()
         await asyncio.sleep(wait_time)
+
 
 @bot.event
 async def on_ready():
     print(f"Loggined in as: {bot.user.name}")
-    #機器人狀態
-    game = nextcord.Game("馬應龍的推特")
-    await bot.change_presence(status=nextcord.Status.online, activity=game)
+    # 機器人狀態
+    act = nextcord.Activity(
+        type=nextcord.ActivityType.listening, name="片恋艶花 - 藤乃靜留"
+    )  # 正在聽
+    # game = nextcord.Game("馬應龍的推特") #正在玩
+    # watch = nextcord.Activity(type=nextcord.ActivityType.watching, name="影片名稱") #正在看
+    # stream = nextcord.Streaming(name="直播間", url=jdata["twitch_url"]) #正在直播
+    await bot.change_presence(status=nextcord.Status.online, activity=act)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot.run(os.getenv("BOT_TOKEN"))
--- tweepy_setup.py
+++ tweepy_setup.py
@@ -1,13 +1,14 @@
-import os
+import os, ast
+from datetime import date
 import tweepy
 from dotenv import load_dotenv
+
 load_dotenv()
 
 consumer_key = os.getenv("API_KEY")
 consumer_secret = os.getenv("API_KEY_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
 
 # 設置 tweepy API 密鑰和權限
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,30 +17,67 @@ auth.set_access_token(access_token, access_token_secret)
 # 創建 tweepy API 實例
 api = tweepy.API(auth)
 
-
 keyword_dict = {}
+with open("last_search_result.txt", "r") as last_search_result:
+    string_search_result = last_search_result.read()
+    if string_search_result:
+        keyword_dict = ast.literal_eval(string_search_result)
 
-def search_tweets(keyword, faves):
-    search_results = []
-    #使用 tweepy 進行推特搜索
-    search_word = f"{keyword} min_faves:{faves} filter:media -filter:replies -filter:retweets"
-    for tweets in api.search_tweets(q=search_word, result_type="recent", count=100):
+
+def search_tweets(keyword, exclude_words, faves):
+
+    # 使用 tweepy 進行推特搜索
+    today = date.today()
+    before_3_day = today.replace(day=today.day - 2)
+    search_word = f"{keyword} min_faves:{faves} filter:media since:{before_3_day} -filter:replies -filter:retweets"
+    search_results = api.search_tweets(q=search_word, result_type="recent", count=100)
+    filtered_results = []
+
+    # 排除指定關鍵字內容，如果有輸入排除關鍵字才執行
+    if exclude_words[0]:
+        for tweets in search_results.copy():
+            for exclude in exclude_words:
+                if exclude in tweets.text:
+                    search_results.remove(tweets)
+                    break
+
+    for tweets in search_results:
         human_time = str(tweets.created_at).split("+")[0]
-        tweet_result_format = f"{human_time}  |  https://twitter.com/user/status/{tweets.id}"
-        search_results.insert(0, tweet_result_format)
-    return search_results
+        tweet_result_format = (
+            f"{human_time}  |  https://twitter.com/user/status/{tweets.id}"
+        )
+        filtered_results.insert(0, tweet_result_format)
 
-def check_tweets(keyword, faves):
-    search_results = search_tweets(keyword, faves)
-    if keyword not in keyword_dict:
-        #第一次搜尋
-        keyword_dict[keyword] = search_results
+    return filtered_results
+
+
+def check_tweets(room_id, keyword, exclude_words, faves):
+    room_id = str(room_id)
+    search_results = search_tweets(keyword, exclude_words, faves)
+
+    if room_id not in keyword_dict:
+        # 第一次搜尋
+        keyword_dict[room_id] = search_results
+        with open("last_search_result.txt", "w") as last_search_result:
+            last_search_result.write(str(keyword_dict))
+
         return search_results
     else:
-        #第二次或以上搜尋，篩選掉已經發過的內容
-        filtered_search_results = search_results.copy()
-        for tweet in keyword_dict[keyword]:
-            filtered_search_results.remove(tweet)
-            
-        keyword_dict[keyword] = search_results
-        return filtered_search_results
+        # 第二次或以上搜尋
+        new_search_results = search_results.copy()
+        for tweet in keyword_dict[room_id]:
+            # 舊推文存在在新推文的話要移除
+            if tweet in new_search_results:
+                new_search_results.remove(tweet)
+
+        keyword_dict[room_id] = search_results
+        with open("last_search_result.txt", "w") as last_search_result:
+            last_search_result.write(str(keyword_dict))
+
+        return new_search_results
+
+
+# 將搜尋關鍵字資料移除
+def stop_tweets(room_id):
+    room_id = str(room_id)
+    keyword_dict.pop(room_id)
